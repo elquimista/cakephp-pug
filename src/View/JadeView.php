81ce59deeab017e669c65b9d0421ddfd6a8f2795
clthck <joey.corleone92@yahoo.com>
2016-01-04 21:26:16 +0300

    Initial Commit

---
+++ src/View/JadeView.php
@@ -0,0 +1,150 @@
+<?php
+/**
+ * Copyright (c) clthck (http://github.com/clthck)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright (c) clthck (http://github.com/clthck)
+ * @link      http://github.com/clthck/cakephp-jade
+ * @since     0.0.1
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+namespace clthck\JadePHP;
+
+use Cake\Event\EventManager;
+use Cake\Network\Request;
+use Cake\Network\Response;
+use Cake\View\View;
+use Tale\Jade;
+
+/**
+ * Jade View
+ *
+ * View class to preprocess Jade template language
+ *
+ */
+class JadeView extends View
+{
+
+    /**
+     * Constants
+     */
+    const EXT = '.ctp.jade';
+    const TEMPLATE_PATH = APP . 'Template';
+
+    /**
+     * Template file extension.
+     * @override
+     */
+    protected $_ext = self::EXT;
+
+    // Array of file extensions
+    protected $extensions = [
+        self::EXT,
+        '.ctp',
+    ];
+
+    // Instance of Tale\Jade\Renderer
+    protected $renderer;
+
+    /**
+     * Constructor
+     *
+     * @param Request|null       $request
+     * @param Response|null      $response
+     * @param EventManager|null  $eventManager
+     * @param array              $viewOptions
+     */
+    public function __construct(Request $request = null, Response $response = null, EventManager $eventManager = null, array $viewOptions = [])
+    {
+        if ($eventManager === null) {
+            $eventManager = EventManager::instance();
+        }
+
+        $options = [
+            'paths' => [
+                self::TEMPLATE_PATH
+            ],
+            'adapterOptions' => [
+                'path' => CACHE . 'views'
+            ],
+        ];
+
+        $options = array_merge($options, $viewOptions);
+
+        $this->renderer = new Jade\Renderer($options);
+
+        parent::__construct($request, $response, $eventManager, $viewOptions);
+        $this->_ext = self::EXT;
+    }
+
+    /**
+     * Renders and returns output for given template filename with its array of data.
+     * Handles parent/extended templates.
+     *
+     * @param string    $viewFile
+     * @param array     $data
+     * @return string
+     */
+    protected function _render($viewFile, $data = [])
+    {
+        if (empty($data)) {
+            $data = $this->viewVars;
+        }
+
+        if (substr($viewFile, -3) === 'ctp') {
+            $out = parent::_render($viewFile, $data);
+        } else {
+            $viewFile = str_replace(self::TEMPLATE_PATH, '', $viewFile);
+            $out = $this->renderer->render($viewFile, $data);
+        }
+        
+        return $out;
+    }
+
+    /**
+     * Returns filename of given action's template file (.ctp.jade) as a string.
+     * CamelCased action names will be under_scored by default.
+     * This means that you can have LongActionNames that refer to long_action_names.ctp.jade view.
+     * You can change the inflection rule by overriding _inflectViewFileName.
+     *
+     * @param string|null $name
+     * @return string
+     */
+    protected function _getViewFileName($name = null)
+    {
+        $rethrow = new \Exception("You're not supposed to get here.");
+        foreach ($this->extensions as $extension) {
+            $this->_ext = $extension;
+            try {
+                return parent::_getViewFileName($name);
+            } catch (\Exception $e) {
+                $rethrow = $e;
+            }
+        }
+
+        throw $rethrow;
+    }
+
+    /**
+     * Returns layout filename for this template as a string.
+     *
+     * @param string|null $name
+     * @return string
+     */
+    protected function _getLayoutFileName($name = null)
+    {
+        $rethrow = new \Exception("You're not supposed to get here.");
+        foreach ($this->extensions as $extension) {
+            $this->_ext = $extension;
+            try {
+                return parent::_getLayoutFileName($name);
+            } catch (\Exception $e) {
+                $rethrow = $e;
+            }
+        }
+
+        throw $rethrow;
+    }
+}
