762bf2152a98072195f99ffbe76125a6413da30e
clthck <joey.corleone92@yahoo.com>
2016-01-17 00:07:00 +0300

    Remove default lexerOptions again

--- src/View/JadeView.php
+++ src/View/JadeView.php
@@ -78,10 +78,6 @@ class JadeView extends View
             'adapterOptions' => [
                 'path' => CACHE . 'views'
             ],
-            'lexerOptions' => [
-                'indentStyle' => ' ',
-                'indentWidth' => 2,
-            ]
         ];
         $options = array_merge($options, $this->viewOptions());
 
